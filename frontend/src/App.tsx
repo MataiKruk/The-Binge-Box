ed65b37db167e35bfed9fabd412f4f4416fd01e3
tibz <shaveddoor@gmail.com>
2024-03-24 16:17:34 -0400

    signin update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Search from "./components/Search/Search";
 import AdvancedSearch from "./components/AdvancedSearch/AdvancedSearch";
 import MovieDetails from "./components/MovieDetails/MovieDetails";
@@ -13,14 +8,20 @@ import SignIn from "./components/SignIn/SignIn";
 import MataiPlaylist from "./components/MataiPlaylist/MataiPlaylist";
 import AyeshaPlaylist from "./components/AyeshaPlaylist/AyeshaPlaylist";
 import MohammadPlaylist from "./components/MohammadPlaylist/MohammadPlaylist";
+import Header from "./components/Header/Header";
+import SignInForm from "./components/SignIn/SignInForm";
+import SignUpForm from "./components/SignUp/SignUpForm";
 
 function App() {
   return (
     <>
       <div className="App">
         <Router>
+          <Header />
           <Routes>
             <Route path="/" element={<SignIn />} />
+            <Route path="/sign-in" element={<SignInForm />} />
+            <Route path="/sign-up" element={<SignUpForm />} />
             <Route path="/home" element={<Home />} />
             <Route path="/search" element={<Search />} />
             <Route
--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -1,39 +1,12 @@
 import React from "react";
+import { useAuth, useUser } from "../../context/AuthContext";
 import "./SignIn.css";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebaseConfig";
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const Main: React.FC = () => {
   const navigate = useNavigate();
-
-  const handleSignIn = () => {
-    console.log("Signing in...");
-  };
-
-  const handleSignUp = () => {
-    console.log("Navigating to sign up...");
-  };
-
-  const signInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      navigate("/home");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const signOutUser = async () => {
-    try {
-      await signOut(auth);
-      console.log("Signed out successfully!");
-      navigate("/");
-    } catch (error) {
-      console.error("Sign out error:", error);
-    }
-  };
+  const user = useUser();
+  const { signInWithGoogle, signOutUser } = useAuth();
 
   return (
     <div className="signin-container">
@@ -41,20 +14,25 @@ const Main: React.FC = () => {
         <img src="/images/logo.png" alt="Logo" className="logo" />
       </div>
       <div className="buttons-container">
-        <button onClick={handleSignIn} className="signin-btn">
-          Sign In
-        </button>
-        <button onClick={handleSignUp} className="signup-btn">
-          Sign Up
-        </button>
+        {!user ? (
+          <>
+            <button onClick={() => navigate("/sign-in")} className="signin-btn">
+              Sign In
+            </button>
 
-        <button className="google-signin-btn" onClick={signInWithGoogle}>
-          Sign in with Google
-        </button>
+            <button onClick={() => navigate("/sign-up")} className="signup-btn">
+              Sign Up
+            </button>
 
-        <button onClick={signOutUser} className="signout-btn">
-          Sign Out
-        </button>
+            <button className="google-signin-btn" onClick={signInWithGoogle}>
+              Sign in with Google
+            </button>
+          </>
+        ) : (
+          <button onClick={signOutUser} className="signout-btn">
+            Sign Out
+          </button>
+        )}
       </div>
     </div>
   );
