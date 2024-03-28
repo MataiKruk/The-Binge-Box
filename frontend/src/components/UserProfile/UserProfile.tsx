bac0154b4e4e8b8e696529f082b02d269c5dd778
tibz <shaveddoor@gmail.com>
2024-03-28 18:41:38 -0400

    user-profile-avatar

--- frontend/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -1,15 +1,28 @@
-import './UserProfile.css';
-import StickyFooter from '../StickyFooter/StickyFooter';
-import UserPlaylistList from '../UserPlaylistList/UserPlaylistList';
+import "./UserProfile.css";
+import StickyFooter from "../StickyFooter/StickyFooter";
+import UserPlaylistList from "../UserPlaylistList/UserPlaylistList";
+import { useUser } from "../../context/AuthContext";
+import { Avatar } from "../Avatar/Avatar";
 
 const UserProfile = () => {
-    return (
-        <div>
-        <p>USERPROFILE COMPONENT</p>
-        <UserPlaylistList></UserPlaylistList>
-        <StickyFooter></StickyFooter>
-        </div>
-    )
-}
+  const user = useUser();
+
+  return (
+    <div>
+      <div className="user-profile-header">
+        <Avatar
+          size="xxlarge"
+          src={user?.photoURL ?? undefined}
+          alt={user?.displayName ?? undefined}
+        />
+        <h6 className="user-profile-username">
+          {user?.displayName ?? user?.email}
+        </h6>
+      </div>
+      <UserPlaylistList></UserPlaylistList>
+      <StickyFooter></StickyFooter>
+    </div>
+  );
+};
 
 export default UserProfile;
